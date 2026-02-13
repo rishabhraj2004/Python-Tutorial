0bd8fb6a9a5f60a0ca1045fe86133c721c9a5c24
rishabhraj2004 <rishabhraj5691@gmail.com>
2026-02-13 12:32:31 +0530

    adding input taking from user

---
+++ input_in_python.py
@@ -0,0 +1,5 @@
+name = input("enter your name:....") #input function is used to take input from the user   
+print("welcome", name)
+age = int(input("enter your age:....")) #input function takes input in string format so we have to convert it into integer using int() function
+print("your age is", age)
+print(type(age),age) #type of variable is here int cz we change the type of variable from string to integer using int() function
