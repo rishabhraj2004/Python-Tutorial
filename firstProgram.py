2a8e77b210baed891fb6e436530f5fcfed5356ac
rishabhraj2004 <rishabhraj5691@gmail.com>
2026-02-13 10:54:12 +0530

    variable & data type

---
+++ firstProgram.py
@@ -0,0 +1,10 @@
+print("Akku")
+print("My self Rishu")
+print("I am learning Python")
+print("I am enjoying it\nAnd I am loving it")
+print("I am learning Python, And I am loving it")
+
+
+
+print(35)
+print(98+69)
---
+++ variable.py
@@ -0,0 +1,23 @@
+name = "Rishu"  #strings can use under single or double quots( '', "")
+age = 21  #integers
+price = 728640.326  #floating value
+
+age3 = age - 1  #age is now 24
+age2 = age #store the value of age in age2 variable
+
+
+print(name)
+print(age)
+print(price)
+
+#for checking the data type of variable we use type() function
+print(type(name)) 
+print(type(age))
+print(type(price))
+
+ #string concatenation
+print("My name is ", name) 
+print("My age is ", age)
+print("The price of the laptop is ", price)
+print("akku age is", age2)
+print("ganesh age is", age3)
